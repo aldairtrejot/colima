694d596b0d18520182bb38de19ffa33ce8958f4f
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-08-30 11:30:45 -0600

    on changes

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 $password = "pg2024";
 $username = "postgres";
-$dbname = "colima_produccion";
+$dbname = "colima_produccion*";
 $host = "localhost";
 $port = "5432";
 $options = "--client_encoding=UTF8";
